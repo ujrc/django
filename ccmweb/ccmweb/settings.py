bd8bd4bc5a40098147c7de8230e7681cb44a932e
ujrc <uwjearc@yahoo.com>
2016-05-15 21:10:31 -0500

    removing deleted files from tracking

--- ccmweb/ccmweb/settings.py
+++ ccmweb/ccmweb/settings.py
@@ -10,14 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-# import os
+import os
 
 # # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-import os
+# import os
 from django.core.exceptions import ImproperlyConfigured
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Handling Key Import Errors
 def get_env_variable(var_name):
