54af3e5df098dbc824b30b533c9e4743296eebad
ujrc <uwjearc@yahoo.com>
2016-05-17 12:21:00 -0500

    created accounts app and account list URL

--- ccmweb/ccmweb/urls.py
+++ ccmweb/ccmweb/urls.py
@@ -19,12 +19,18 @@ from django.conf.urls import url
 from django.contrib import admin
 from marketing.views import HomePage
 from subscribers import views as subscribers_view
+
+from accounts.views import AccountList
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',HomePage.as_view(),name='home'),
     url(r'^signup/$',subscribers_view.subscriber_new,name='sub_new'),
     url(r'^login/$','django.contrib.auth.views.login',{'template_name':'login.html'}),
     url(r'^logout/$','django.contrib.auth.views.logout',{'next_page':'/login/'}),
+    # Account related URLs
+    url(r'^account/list/$',AccountList.as_view(), name='account_list'
+),
 
 ]
 
