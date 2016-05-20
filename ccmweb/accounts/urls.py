6e212b516d83aa32048809bd5ebafaa680107cd3
ujrc <uwjearc@yahoo.com>
2016-05-20 09:37:54 -0500

    change directory

--- ccmweb/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from  accounts import  views as account_view
-
-account_urls=[
-	url(r'^$',account_view.account_detail,name='account_detail'),
-	url(r'^edit/$',account_view.account_cru,name='account_update'),
-
-]
