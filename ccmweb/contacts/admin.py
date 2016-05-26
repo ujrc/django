637e60629515ceddf179a2b6b585812bc5668b4a
ujrc <uwjearc@yahoo.com>
2016-05-25 22:32:21 -0500

    Modified communications app

--- ccmweb/contacts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Contact
-
-class ContactAdmin(admin.ModelAdmin):
-	list_display=['first_name','last_name','uuid']
-
-admin.site.register(Contact,ContactAdmin)
