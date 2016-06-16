71252c59644ff0ec89bf662f3cf6e3079520c5b0
ujrc <uwjearc@yahoo.com>
2016-06-16 15:15:09 -0500

    Created base,home,login and logout templates

--- learning/elearning/elearning/settings.py
+++ learning/elearning/elearning/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'elearning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,3 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),'static_cdn')
+
+MEDIA_URL='/media/'
+MEDIA_ROOT =os.path.join(os.path.dirname(BASE_DIR),'media_cdn')
+
+
--- learning/elearning/elearning/urls.py
+++ learning/elearning/elearning/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url,include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+	url(r'^$',TemplateView.as_view(template_name='home.html'),name='homepage'),
+	url(r'^accounts/login/$', auth_views.login,name='login'),
+	url(r'^accounts/logout/$', auth_views.logout,name='logout'),
+
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+	urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
